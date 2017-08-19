ab52dd0819dc41e4e7a5c963dcefafe65ae5f23f
dpedroza <dpedroza@CRP1OFFICE23>
2017-08-18 23:31:51 -0700

    Add project files.

---
+++ ReadyPlayer1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ReadyPlayer1.MvcApplication" Language="C#" %>
---
+++ ReadyPlayer1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ReadyPlayer1.Startup))]
+namespace ReadyPlayer1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
